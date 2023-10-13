78646bb5b06631489a0424d23bdeff599e6e16a9
eivmosn <eivmosn@163.com>
2023-10-13 23:51:43 +0800

    feat: add button toolbar

--- src/components/others/Toolbar.tsx
+++ src/components/others/Toolbar.tsx
@@ -1,3 +1,4 @@
+import { NButton } from 'naive-ui'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -8,7 +9,10 @@ export default defineComponent({
   render() {
     return (<div class="min-h-48px fb b-bottom b-[var(--x-border)] bg-[var(--x-background-light)] px-5px">
       <div class="flex gap-8px">
-
+        <NButton>Export</NButton>
+        <NButton>Reset</NButton>
+        <NButton>Import</NButton>
+        <NButton>Preview</NButton>
       </div>
     </div>)
   },
