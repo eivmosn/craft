863316786df347d55255ae5a53a794799d6b05da
eivmosn <eivmosn@163.com>
2023-10-24 18:28:21 +0800

    fix: border card for component

--- src/app/components/FcMaterials.tsx
+++ src/app/components/FcMaterials.tsx
@@ -11,9 +11,42 @@ export default defineComponent({
   render() {
     return (<div class="min-w-270px w-270px b-right">
       <Paneview title="组件">
-        <Collapse />
-        <Collapse />
-        <Collapse />
+        <Collapse>
+          {
+            Array.from({ length: 13 }).map(() => <div class="min-h-89.66px bg-[var(--fc-background-light)]"></div>)
+          }
+          <div
+            style={{
+              minHeight: '89.66px',
+              gridColumnStart: 'span 2',
+              background: 'var(--fc-background-light)',
+            }}
+          />
+        </Collapse>
+        <Collapse>
+          {
+            Array.from({ length: 13 }).map(() => <div class="min-h-89.66px bg-[var(--fc-background-light)]"></div>)
+          }
+          <div
+            style={{
+              minHeight: '89.66px',
+              gridColumnStart: 'span 2',
+              background: 'var(--fc-background-light)',
+            }}
+          />
+        </Collapse>
+        <Collapse>
+          {
+            Array.from({ length: 3 }).map(() => <div class="min-h-89.66px bg-[var(--fc-background-light)]"></div>)
+          }
+          <div
+            style={{
+              minHeight: '89.66px',
+              gridColumnStart: 'span 3',
+              background: 'var(--fc-background-light)',
+            }}
+          />
+        </Collapse>
       </Paneview>
     </div>)
   },
--- src/app/misc/Collapse.tsx
+++ src/app/misc/Collapse.tsx
@@ -10,8 +10,8 @@ export default defineComponent({
   render() {
     return (<NCollapse defaultExpandedNames={['comp']}>
       <NCollapseItem class="fc-collapse" title="组件" name="comp">
-        <div class="fc-collapse-item b-bottom">
-
+        <div class="fc-collapse-item grid grid-cols-[repeat(3,33.33%)] gap-1px b-bottom bg-[var(--fc-border)]">
+          {this.$slots.default?.()}
         </div>
       </NCollapseItem>
     </NCollapse>)
