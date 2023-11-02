dfa8c522caff306efdeab7e65f5b202bf16e95d3
eivmosn <eivmosn@163.com>
2023-11-02 23:49:22 +0800

    feat: navbar icon

--- src/app/components/FcNav.tsx
+++ src/app/components/FcNav.tsx
@@ -1,4 +1,5 @@
 import { defineComponent } from 'vue'
+import icon from '../icon.config'
 
 export default defineComponent({
   inheritAttrs: false,
@@ -7,8 +8,12 @@ export default defineComponent({
 
   },
   render() {
-    return (<div class="min-w-48px w-48px fc flex-col justify-end gap-22px b-right py-20px">
-      {/* <div v-html={icon.alert} class="fc"></div> */}
-    </div>)
+    return (
+      <div class="min-w-48px w-48px fc flex-col justify-start gap-22px b-right py-20px text-#333">
+        <div v-html={icon.puzzle} class="fc"></div>
+        <div v-html={icon.layout} class="fc"></div>
+        <div v-html={icon.setting} class="fc"></div>
+      </div>
+    )
   },
 })
