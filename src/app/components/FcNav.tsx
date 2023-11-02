eb343897779187cadd88eaf5ea2f8bbd3c59c5e4
eivmosn <eivmosn@163.com>
2023-11-02 23:58:16 +0800

    feat: navbar indicator design

--- src/app/components/FcNav.tsx
+++ src/app/components/FcNav.tsx
@@ -10,9 +10,11 @@ export default defineComponent({
   render() {
     return (
       <div class="min-w-48px w-48px fc flex-col justify-start gap-22px b-right py-20px text-#333">
-        <div v-html={icon.puzzle} class="fc"></div>
-        <div v-html={icon.layout} class="fc"></div>
-        <div v-html={icon.setting} class="fc"></div>
+        <div v-html={icon.puzzle} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-blue content-empty -bottom-7px)"></div>
+        <div v-html={icon.layout} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-red content-empty -bottom-7px)"></div>
+        <div v-html={icon.setting} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-green content-empty -bottom-7px)"></div>
+        <div v-html={icon.puzzle} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-green content-empty -bottom-7px)"></div>
+        <div v-html={icon.setting} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-green content-empty -bottom-7px)"></div>
       </div>
     )
   },
