7e5338f6b92dfb77903fc51fc9fedb48e5f9b531
eivmosn <eivmosn@163.com>
2023-11-03 00:35:07 +0800

    style: change icon size

--- src/app/components/FcNav.tsx
+++ src/app/components/FcNav.tsx
@@ -1,20 +1,44 @@
 import { defineComponent } from 'vue'
+import { NTooltip } from 'naive-ui'
 import icon from '../icon.config'
+
+const navs = [
+  {
+    name: 'Components',
+    icon: icon.puzzle,
+  },
+  {
+    name: 'Layers',
+    icon: icon.layers,
+  },
+  {
+    name: 'Schema',
+    icon: icon.code,
+  },
+  {
+    name: 'Settings',
+    icon: icon.setting,
+  },
+]
 
 export default defineComponent({
   inheritAttrs: false,
   name: 'FcNav',
-  setup() {
-
-  },
   render() {
     return (
-      <div class="min-w-48px w-48px fc flex-col justify-start gap-22px b-right py-20px text-#333">
-        <div v-html={icon.puzzle} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-blue content-empty -bottom-7px)"></div>
-        <div v-html={icon.layout} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-red content-empty -bottom-7px)"></div>
-        <div v-html={icon.setting} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-green content-empty -bottom-7px)"></div>
-        <div v-html={icon.puzzle} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-green content-empty -bottom-7px)"></div>
-        <div v-html={icon.setting} class="relative fc after:(absolute left-5px right-0 h-3px w-18px b-rd-full bg-green content-empty -bottom-7px)"></div>
+      <div class="min-w-48px w-48px fc flex-col justify-start gap-22px b-right py-20px text-#666">
+        {
+          navs.map(nav => (
+            <NTooltip
+              placement="right"
+              v-slots={{
+                trigger: () => <div v-html={nav.icon} class="relative fc hover:after:(absolute h-12px w-3px b-rd-full bg-blue content-empty -left-7px)" />,
+              }}
+            >
+              {nav.name}
+            </NTooltip>
+          ))
+        }
       </div>
     )
   },
