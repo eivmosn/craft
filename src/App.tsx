7638214734a94b78810f53d4ee5523827634d60f
eivmosn <eivmosn@163.com>
2023-11-07 23:56:38 +0800

    chore: collapse svg icon

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,30 @@ import { indentMore } from '@codemirror/commands'
 import { json } from '@codemirror/lang-json'
 import { defaultHighlightStyle, foldGutter, syntaxHighlighting } from '@codemirror/language'
 
+interface PathAttributes {
+  [key: string]: string
+}
+
+interface SVGOptions {
+  width: string
+  height: string
+  viewBox: string
+  pathAttributes: PathAttributes
+}
+
+function createSVGPath(options: SVGOptions): SVGElement {
+  const { width, height, viewBox, pathAttributes } = options
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  svg.setAttribute('width', width)
+  svg.setAttribute('height', height)
+  svg.setAttribute('viewBox', viewBox)
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  for (const [key, value] of Object.entries(pathAttributes))
+    path.setAttribute(key, value)
+  svg.appendChild(path)
+  return svg
+}
+
 export function useEditor(options: {
   container: HTMLElement
   lineNumbers?: boolean
@@ -49,19 +73,38 @@ export function useEditor(options: {
         },
       }),
       foldGutter({
-        markerDOM: () => {
+        markerDOM: (open) => {
           const icon = document.createElement('div')
-          icon.style.cssText = 'display: flex; align-items: center; justify-content: center;'
-          const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-          svg.setAttribute('width', '20')
-          svg.setAttribute('height', '20')
-          svg.setAttribute('viewBox', '0 0 24 24')
-          const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-          path.setAttribute('fill', 'currentColor')
-          path.setAttribute('d', 'M8.59 16.59L13.17 12L8.59 7.41L10 6l6 6l-6 6l-1.41-1.41')
-          svg.appendChild(path)
-          icon.appendChild(svg)
-          return icon
+          const icon2 = document.createElement('div')
+          icon2.className = 'cm-gutterElement'
+          icon.className = 'cm-gutterElement'
+          const svgRight = createSVGPath({
+            width: '15',
+            height: '15',
+            viewBox: '0 0 24 24',
+            pathAttributes: {
+              'fill': 'none',
+              'stroke': 'currentColor',
+              'stroke-linecap': 'round',
+              'stroke-linejoin': 'round',
+              'stroke-width': '1.5',
+              'd': 'm9 5l6 7l-6 7',
+            },
+          })
+          const svgDown = createSVGPath({
+            width: '15',
+            height: '15',
+            viewBox: '0 0 24 24',
+            pathAttributes: {
+              'fill': 'currentColor',
+              'fill-rule': 'evenodd',
+              'd': 'M4.43 8.512a.75.75 0 0 1 1.058-.081L12 14.012l6.512-5.581a.75.75 0 0 1 .976 1.138l-7 6a.75.75 0 0 1-.976 0l-7-6a.75.75 0 0 1-.081-1.057Z',
+              'clip-rule': 'evenodd',
+            },
+          })
+          icon.appendChild(svgDown)
+          icon2.appendChild(svgRight)
+          return open ? icon : icon2
         },
       }),
       syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
