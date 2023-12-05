3ced54c3d7d78982a968e25b84ab793a2ba33d61
eivmosn <eivmosn@163.com>
2023-12-06 01:09:11 +0800

    feat: complete widget list

--- src/view/Menubar.tsx
+++ src/view/Menubar.tsx
@@ -50,16 +50,6 @@ export default defineComponent({
             <path fill="currentColor" d="m31 16l-7 7l-1.41-1.41L28.17 16l-5.58-5.59L24 9l7 7zM1 16l7-7l1.41 1.41L3.83 16l5.58 5.59L8 23l-7-7zm11.42 9.484L17.64 6l1.932.517L14.352 26z" />
           </svg>
         </MenuItem>
-        <MenuItem text="主题">
-          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32">
-            <circle cx="10" cy="12" r="2" fill="currentColor" />
-            <circle cx="16" cy="9" r="2" fill="currentColor" />
-            <circle cx="22" cy="12" r="2" fill="currentColor" />
-            <circle cx="23" cy="18" r="2" fill="currentColor" />
-            <circle cx="19" cy="23" r="2" fill="currentColor" />
-            <path fill="currentColor" d="M16.54 2A14 14 0 0 0 2 16a4.82 4.82 0 0 0 6.09 4.65l1.12-.31a3 3 0 0 1 3.79 2.9V27a3 3 0 0 0 3 3a14 14 0 0 0 14-14.54A14.05 14.05 0 0 0 16.54 2Zm8.11 22.31A11.93 11.93 0 0 1 16 28a1 1 0 0 1-1-1v-3.76a5 5 0 0 0-5-5a5.07 5.07 0 0 0-1.33.18l-1.12.31A2.82 2.82 0 0 1 4 16A12 12 0 0 1 16.47 4A12.18 12.18 0 0 1 28 15.53a11.89 11.89 0 0 1-3.35 8.79Z" />
-          </svg>
-        </MenuItem>
       </div>
     )
   },
--- src/view/WidgetPanel.tsx
+++ src/view/WidgetPanel.tsx
@@ -1,6 +1,5 @@
 import { defineComponent } from 'vue'
-import { NCollapse, NCollapseItem, NInput } from 'naive-ui'
-import { Input, Password, Select, TextArea } from '../element'
+import { Checkbox, ColorPicker, Date, HTML, IconPicker, Image, Input, InputNumber, Link, Password, QRCode, Radio, Rate, Select, TextArea, Time } from '../element'
 
 export default defineComponent({
   inheritAttrs: false,
@@ -9,50 +8,35 @@ export default defineComponent({
 
   },
   render() {
-    // const SearchArea = () => {
-    //   return (
-    //     <div class="b-bottom b-[var(--border-color)] p-6px">
-    //       <NInput
-    //         placeholder="搜索组件"
-    //         v-slots={{
-    //           suffix: () => (
-    //             <svg class="text-[var(--text-color-3)]" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 32 32">
-    //               <path fill="currentColor" d="m29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29ZM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9Z" />
-    //             </svg>
-    //           ),
-    //         }}
-    //       />
-    //     </div>
-    //   )
-    // }
-
-    // const WidgetCard = () => {
-    //   return (
-    //     <div class="flex flex-col h-60px b b-rd-2px b-[var(--divider-color)] bg-[var(--action-color)] hover:bg-[var(--hover-color)]">
-    //       <div class="flex-1 fc">
-    //         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="M2 28h28v2H2zm22.5-17H8a2.002 2.002 0 0 0-2 2v8a5.006 5.006 0 0 0 5 5h8a5.006 5.006 0 0 0 5-5v-1h.5a4.5 4.5 0 0 0 0-9zM22 21a3.003 3.003 0 0 1-3 3h-8a3.003 3.003 0 0 1-3-3v-8h14zm2.5-3H24v-5h.5a2.5 2.5 0 0 1 0 5zM19 9h-2v-.146a1.988 1.988 0 0 0-1.105-1.789L13.21 5.724A3.979 3.979 0 0 1 11 2.146V1h2v1.146a1.99 1.99 0 0 0 1.105 1.789l2.684 1.341A3.98 3.98 0 0 1 19 8.854z" /></svg>
-    //       </div>
-    //       <div class="py-2px px-5px text-12px text-center">组件名称</div>
-    //     </div>
-    //   )
-    // }
-
     return (
-      <div class="w-213px select-none b-right b-[var(--border-color)] bg-[var(--card-color)]">
-        {
-          Array.from({ length: 1 }).map(() => (
-            <NCollapse arrowPlacement="right" defaultExpandedNames={['1']}>
-              <NCollapseItem class="fc-collapse" title="容器" name="1">
-                {/* <div class="grid p-8px grid-cols-3 gap-8px">
-                  {
-                    [Input, TextArea, Password, Select].map(WidgetCard)
-                  }
-                </div> */}
-                <div class="min-h-20px"></div>
-              </NCollapseItem>
-            </NCollapse>
-          ))
-        }
+      <div class="w-263px select-none b-right b-[var(--border-color)] bg-[var(--card-color)] p-8px">
+        <div class="grid grid-cols-1 gap-8px">
+          {
+            [
+              Input,
+              TextArea,
+              InputNumber,
+              Password,
+              Select,
+              Date,
+              Time,
+              Rate,
+              Image,
+              IconPicker,
+              Checkbox,
+              Radio,
+              QRCode,
+              HTML,
+              Link,
+              ColorPicker,
+            ].map(widget => (
+              <div class="p-4px fs gap-10px px-8px b-rd-3px hover:bg-[var(--hover-color)]">
+                <div class="fc text-blue-5" v-html={widget.icon} />
+                <div class="text-[var(--text-color-2)]">{widget.label['zh-CN']}</div>
+              </div>
+            ))
+          }
+        </div>
       </div>
     )
   },
