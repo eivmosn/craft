fb9b381c97e59e43c65911a49bb825aa406af7d1
eivmosn <eivmosn@163.com>
2023-12-05 23:45:26 +0800

    chore: file change

---
+++ src/element.ts
@@ -0,0 +1,59 @@
+export const Input = {
+  type: 'Input',
+  icon: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M21 3.01H3c-1.1 0-2 .9-2 2V8c0 .55.45 1 1 1s1-.45 1-1V5.99c0-.55.45-1 1-1h16c.55 0 1 .45 1 1v12.03c0 .55-.45 1-1 1H4c-.55 0-1-.45-1-1V16c0-.55-.45-1-1-1s-1 .45-1 1v3.01c0 1.09.89 1.98 1.98 1.98H21c1.1 0 2-.9 2-2V5.01c0-1.1-.9-2-2-2zm-9.15 12.14l2.79-2.79c.2-.2.2-.51 0-.71l-2.79-2.79a.495.495 0 0 0-.85.35V11H2c-.55 0-1 .45-1 1s.45 1 1 1h9v1.79c0 .45.54.67.85.36z" /></svg>',
+  label: {
+    'en-US': 'Input',
+    'zh-CN': '输入框',
+  },
+  props: {
+
+  },
+}
+
+export const InputNumber = {
+  type: 'InputNumber',
+  icon: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="currentColor" d="M508 280h-63.3c-3.3 0-6 2.7-6 6v340.2H433L197.4 282.6c-1.1-1.6-3-2.6-4.9-2.6H126c-3.3 0-6 2.7-6 6v464c0 3.3 2.7 6 6 6h62.7c3.3 0 6-2.7 6-6V405.1h5.7l238.2 348.3c1.1 1.6 3 2.6 5 2.6H508c3.3 0 6-2.7 6-6V286c0-3.3-2.7-6-6-6zm378 413H582c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h304c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm-152.2-63c52.9 0 95.2-17.2 126.2-51.7c29.4-32.9 44-75.8 44-128.8c0-53.1-14.6-96.5-44-129.3c-30.9-34.8-73.2-52.2-126.2-52.2c-53.7 0-95.9 17.5-126.3 52.8c-29.2 33.1-43.4 75.9-43.4 128.7c0 52.4 14.3 95.2 43.5 128.3c30.6 34.7 73 52.2 126.2 52.2zm-71.5-263.7c16.9-20.6 40.3-30.9 71.4-30.9c31.5 0 54.8 9.6 71 29.1c16.4 20.3 24.9 48.6 24.9 84.9c0 36.3-8.4 64.1-24.8 83.9c-16.5 19.4-40 29.2-71.1 29.2c-31.2 0-55-10.3-71.4-30.4c-16.3-20.1-24.5-47.3-24.5-82.6c.1-35.8 8.2-63 24.5-83.2z"/></svg>',
+  label: {
+    'en-US': 'InputNumber',
+    'zh-CN': '数字框',
+  },
+  props: {
+
+  },
+}
+
+export const TextArea = {
+  type: 'TextArea',
+  icon: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" d="M0 4.5A2.5 2.5 0 0 1 2.5 2h11A2.5 2.5 0 0 1 16 4.5v7a2.5 2.5 0 0 1-2.5 2.5h-11A2.5 2.5 0 0 1 0 11.5zM2.5 3A1.5 1.5 0 0 0 1 4.5v7A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 13.5 3zm10.854 4.646a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708l3-3a.5.5 0 0 1 .708 0zm0 2.5a.5.5 0 0 1 0 .708l-.5.5a.5.5 0 0 1-.708-.708l.5-.5a.5.5 0 0 1 .708 0"/></svg>',
+  label: {
+    'en-US': 'TextArea',
+    'zh-CN': '多行文本',
+  },
+  props: {
+
+  },
+}
+
+export const Password = {
+  type: 'Password',
+  icon: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M12 4h-2C6.229 4 4.343 4 3.172 5.172C2 6.343 2 8.229 2 12c0 3.771 0 5.657 1.172 6.828C4.343 20 6.229 20 10 20h2m3-16c3.114.01 4.765.108 5.828 1.172C22 6.343 22 8.229 22 12c0 3.771 0 5.657-1.172 6.828C19.765 19.892 18.114 19.99 15 20"/><path fill="currentColor" d="M9 12a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm4 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0Z"/><path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M15 2v20"/></g></svg>',
+  label: {
+    'en-US': 'Password',
+    'zh-CN': '密码框',
+  },
+  props: {
+
+  },
+}
+
+export const Select = {
+  type: 'Select',
+  icon: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M26 6V2H2v10h9v18h19V6ZM4 10V4h20v2H11v4Zm24 18H13V8h15Z"/><path fill="currentColor" d="M15 11h2v2h-2zm4 0h7v2h-7zm-4 6h2v2h-2zm4 0h7v2h-7zm-4 6h2v2h-2zm4 0h7v2h-7z"/></svg>',
+  label: {
+    'en-US': 'Select',
+    'zh-CN': '下拉框',
+  },
+  props: {
+
+  },
+}
--- src/view/WidgetPanel.tsx
+++ src/view/WidgetPanel.tsx
@@ -1,5 +1,6 @@
 import { defineComponent } from 'vue'
 import { NCollapse, NCollapseItem, NInput } from 'naive-ui'
+import { Input, Password, Select, TextArea } from '../element'
 
 export default defineComponent({
   inheritAttrs: false,
@@ -8,56 +9,50 @@ export default defineComponent({
 
   },
   render() {
-    const SearchArea = () => {
-      return (
-        <div class="b-bottom b-[var(--border-color)] p-10px">
-          <NInput
-            placeholder="搜索组件"
-            v-slots={{
-              suffix: () => (
-                <svg class="text-[var(--text-color-3)]" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 32 32">
-                  <path fill="currentColor" d="m29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29ZM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9Z" />
-                </svg>
-              ),
-            }}
-          />
-        </div>
-      )
-    }
+    // const SearchArea = () => {
+    //   return (
+    //     <div class="b-bottom b-[var(--border-color)] p-6px">
+    //       <NInput
+    //         placeholder="搜索组件"
+    //         v-slots={{
+    //           suffix: () => (
+    //             <svg class="text-[var(--text-color-3)]" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 32 32">
+    //               <path fill="currentColor" d="m29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29ZM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9Z" />
+    //             </svg>
+    //           ),
+    //         }}
+    //       />
+    //     </div>
+    //   )
+    // }
 
-    const WidgetCard = () => {
-      return (
-        <div class="b b-rd-2px b-[var(--divider-color)] bg-[var(--action-color)] hover:bg-[var(--hover-color)] fc flex-col px-5px py-12px gap-10px">
-          <div class="fc text-blue">
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 32 32"><path fill="currentColor" d="M26 12h-6V6a3.003 3.003 0 0 0-3-3h-2.133a2.01 2.01 0 0 0-1.98 1.717l-.845 5.917L8.465 16H2v14h21a7.008 7.008 0 0 0 7-7v-7a4.005 4.005 0 0 0-4-4ZM8 28H4V18h4Zm20-5a5.006 5.006 0 0 1-5 5H10V17.303l3.958-5.937l.91-6.366H17a1 1 0 0 1 1 1v8h8a2.002 2.002 0 0 1 2 2Z" /></svg>
-          </div>
-          <div class="text-12px text-[var(--text-color-3)]">栅格</div>
-        </div>
-      )
-    }
+    // const WidgetCard = () => {
+    //   return (
+    //     <div class="flex flex-col h-60px b b-rd-2px b-[var(--divider-color)] bg-[var(--action-color)] hover:bg-[var(--hover-color)]">
+    //       <div class="flex-1 fc">
+    //         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="M2 28h28v2H2zm22.5-17H8a2.002 2.002 0 0 0-2 2v8a5.006 5.006 0 0 0 5 5h8a5.006 5.006 0 0 0 5-5v-1h.5a4.5 4.5 0 0 0 0-9zM22 21a3.003 3.003 0 0 1-3 3h-8a3.003 3.003 0 0 1-3-3v-8h14zm2.5-3H24v-5h.5a2.5 2.5 0 0 1 0 5zM19 9h-2v-.146a1.988 1.988 0 0 0-1.105-1.789L13.21 5.724A3.979 3.979 0 0 1 11 2.146V1h2v1.146a1.99 1.99 0 0 0 1.105 1.789l2.684 1.341A3.98 3.98 0 0 1 19 8.854z" /></svg>
+    //       </div>
+    //       <div class="py-2px px-5px text-12px text-center">组件名称</div>
+    //     </div>
+    //   )
+    // }
 
     return (
-      <div class="w-285px select-none b-right b-[var(--border-color)] bg-[var(--card-color)]">
-        {SearchArea()}
-        <NCollapse arrowPlacement="right" defaultExpandedNames={['1']}>
-          <NCollapseItem class="fc-collapse" title="容器" name="1">
-            <div class="grid p-8px grid-cols-3 gap-8px">
-              {
-                Array.from({ length: 6 }).map(WidgetCard)
-              }
-            </div>
-          </NCollapseItem>
-        </NCollapse>
-        <NCollapse arrowPlacement="right" defaultExpandedNames={['1']}>
-          <NCollapseItem class="fc-collapse" title="表单" name="1">
-            <div class="min-h-120px"></div>
-          </NCollapseItem>
-        </NCollapse>
-        <NCollapse arrowPlacement="right" defaultExpandedNames={['1']}>
-          <NCollapseItem class="fc-collapse" title="自定义" name="1">
-            <div class="min-h-120px"></div>
-          </NCollapseItem>
-        </NCollapse>
+      <div class="w-213px select-none b-right b-[var(--border-color)] bg-[var(--card-color)]">
+        {
+          Array.from({ length: 1 }).map(() => (
+            <NCollapse arrowPlacement="right" defaultExpandedNames={['1']}>
+              <NCollapseItem class="fc-collapse" title="容器" name="1">
+                {/* <div class="grid p-8px grid-cols-3 gap-8px">
+                  {
+                    [Input, TextArea, Password, Select].map(WidgetCard)
+                  }
+                </div> */}
+                <div class="min-h-20px"></div>
+              </NCollapseItem>
+            </NCollapse>
+          ))
+        }
       </div>
     )
   },
